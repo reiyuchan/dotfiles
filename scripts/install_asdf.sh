69fc63a9b3f144b15a385f5f565785d54f5e2be8
reiyuchan <therealsyedofficial@gmail.com>
2023-09-18 07:54:28 +0300

    ⬇️ asdf

---
+++ scripts/install_asdf.sh
@@ -0,0 +1,15 @@
+set -eux
+
+version=v0.13.1
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch $version
+
+echo "#asdf configuration" >> $HOME/.bashrc
+echo . "$HOME/.asdf/asdf.sh" >> $HOME/.bashrc
+echo . "$HOME/.asdf/completions/asdf.bash" >> $HOME/.bashrc
+
+asdf plugin add nodejs
+
+asdf install nodejs 18.16.1
+asdf install nodejs 16.9.0
+
