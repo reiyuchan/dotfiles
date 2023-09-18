7f13d2436664be8963afefa9b7434728d8b18d73
reiyuchan <therealsyedofficial@gmail.com>
2023-09-19 01:53:59 +0300

    added zsh install script

---
+++ scripts/install_zsh.sh
@@ -0,0 +1,14 @@
+set -eux
+
+sudo apt-get install zsh
+
+zsh
+
+echo "\n#pyenv configuration" >> $HOME/.zshrc
+echo "export PYENV_ROOT="\$HOME/.pyenv"" >> $HOME/.zshrc
+echo "export PYENV_ROOT="\$HOME/.pyenv"" >> $HOME/.zshrc
+echo "export PYENV_ROOT="\$HOME/.pyenv"" >> $HOME/.zshrc
+echo "eval "\$(pyenv init - --path)"" >> $HOME/.zshrc
+
+echo "\n#n configuration" >> $HOME/.zshrc
+echo "export N_PREFIX="\$HOME/.n"; [[ :\$PATH: == *":\$N_PREFIX/bin:"* ]] || PATH+=":\$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo)." >> $HOME/.zshrc
