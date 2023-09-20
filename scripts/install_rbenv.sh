2a93eb824cfb7e775ec2749637389192f0584e38
reiyuchan <therealsyedofficial@gmail.com>
2023-09-20 06:21:25 +0300

    updated script

--- scripts/install_rbenv.sh
+++ scripts/install_rbenv.sh
@@ -4,7 +4,9 @@ sudo apt install rbenv
 
 if [ -f $HOME/.bashrc ]; then
 	if [ -f $HOME/.zshrc ]; then
+		echo '\n# rbenv configurations' >> $HOME/.zshrc
 		echo 'eval "$(rbenv init -)"' >> $HOME/.zshrc
 	fi
 	echo 'eval "$(rbenv init -)"' >> $HOME/.bashrc
+	echo '\n# rbenv configurations' >> $HOME/.bashrc
 fi
