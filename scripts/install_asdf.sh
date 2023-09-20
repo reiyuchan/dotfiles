d2950c70903db7516692c92b7dacf95e0a2b64d2
reiyuchan <therealsyedofficial@gmail.com>
2023-09-20 06:10:38 +0300

    updated script

--- scripts/install_asdf.sh
+++ scripts/install_asdf.sh
@@ -4,9 +4,15 @@ version=v0.13.1
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch $version
 
-echo '\n#asdf configuration' >> $HOME/.bashrc
-echo . '$HOME/.asdf/asdf.sh' >> $HOME/.bashrc
-echo . '$HOME/.asdf/completions/asdf.bash' >> $HOME/.bashrc
+if [ -f $HOME/.bashrc ]; then
+	echo '\n#asdf configuration' >> $HOME/.bashrc
+	echo . '$HOME/.asdf/asdf.sh' >> $HOME/.bashrc
+	echo . '$HOME/.asdf/completions/asdf.bash' >> $HOME/.bashrc
+elif [ -f $HOME/.zshrc ]; then
+	echo '\n#asdf configuration' >> $HOME/.bashrc
+	echo . '$HOME/.asdf/asdf.sh' >> $HOME/.bashrc
+	echo . '$HOME/.asdf/completions/asdf.bash' >> $HOME/.bashrc
+fi
 
 asdf plugin add nodejs
 
