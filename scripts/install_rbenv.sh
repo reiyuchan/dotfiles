770c7961a309cdae80a211ce5cea92951732103a
reiyuchan <therealsyedofficial@gmail.com>
2023-09-20 06:21:55 +0300

    updated script

--- scripts/install_rbenv.sh
+++ scripts/install_rbenv.sh
@@ -7,6 +7,6 @@ if [ -f $HOME/.bashrc ]; then
 		echo '\n# rbenv configurations' >> $HOME/.zshrc
 		echo 'eval "$(rbenv init -)"' >> $HOME/.zshrc
 	fi
-	echo 'eval "$(rbenv init -)"' >> $HOME/.bashrc
 	echo '\n# rbenv configurations' >> $HOME/.bashrc
+	echo 'eval "$(rbenv init -)"' >> $HOME/.bashrc
 fi
