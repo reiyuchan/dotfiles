b6443959650740069960e58d0320822074e818aa
reiyuchan <iwakura.rei.tk@gmail.com>
2024-04-29 02:28:28 +0300

    fixed infinite loop

--- install.sh
+++ install.sh
@@ -6,13 +6,15 @@
 
 DIR=$(cd $(dirname $0); pwd)
 
-files="$DIR/_*/*.sh"
-for file in $files; do
-	if [ -e $file ] ; then
-		if [ "$(basename $file)" == "install.sh" ]; then
-			continue
-		./"$(basename $file)"
+DEST_DIRS="$DIR/_*"
+file="install.sh"
+for DIR in $DEST_DIRS; do
+	if [ -e $DIR ] ; then
+		cd $DIR
+		if [ -e $file ]; then
+			./install.sh
 		fi
+		cd ..	
 	else
 		echo ERROR:Failed to install..
 		echo "$file" not found
